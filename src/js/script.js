12fee2bda535153c23672803bbfc7feafa090049
Alex Zaslavsky <alex.zaslavsky.ua@gmail.com>
2018-10-03 22:15:03 +0300

    variables-rename

--- src/js/script.js
+++ src/js/script.js
@@ -20,67 +20,67 @@ window.onload = function () {
     };
 
     function slide() {
-        var view = window.innerHeight - 100;
+        const view = window.innerHeight - 100;
 
-        var elementsR = document.querySelectorAll('.right-slide');
+        const elementsR = document.querySelectorAll('.right-slide');
 
-        for (var i = 0; i < elementsR.length; i++) {
+        for (let i = 0; i < elementsR.length; i++) {
             if (elementsR[i].getBoundingClientRect().top < view) {
                 elementsR[i].classList.add('right-animation');
             }
         }
 
-        var elementsL = document.querySelectorAll('.left-slide');
+        const elementsL = document.querySelectorAll('.left-slide');
 
-        for (var i = 0; i < elementsL.length; i++) {
+        for (let i = 0; i < elementsL.length; i++) {
             if (elementsL[i].getBoundingClientRect().top < view) {
                 elementsL[i].classList.add('left-animation');
             }
         }
 
-        var elementsB = document.querySelectorAll('.bottom-slide');
+        const elementsB = document.querySelectorAll('.bottom-slide');
 
-        for (var i = 0; i < elementsB.length; i++) {
+        for (let i = 0; i < elementsB.length; i++) {
             if (elementsB[i].getBoundingClientRect().top < view) {
                 elementsB[i].classList.add('bottom-animation');
             }
         }
-    };
+    }
 
     // ---Ripple-effect on main links click---
 
-    var links = document.querySelectorAll('a');
+    const links = document.querySelectorAll('a');
 
-    for (var i = 0; i < links.length; i++) {
+    for (let i = 0; i < links.length; i++) {
         links[i].addEventListener('click', createRipple);
     }
 
     function createRipple(e) {
-        var circle = document.createElement('span');
+        const circle = document.createElement('span');
         this.appendChild(circle);
 
         circle.style.left = e.clientX - this.getBoundingClientRect().left + 'px';
         circle.style.top = e.clientY - this.getBoundingClientRect().top + 'px';
 
         circle.classList.add("ripple");
-    };
+    }
 
     // --- RADIO INPUTS FOR FEEDBACK ---
 
     function getInputs() {
-        var clients = document.querySelectorAll('.client');
-        var inputsQuantity = 0;
+        const clients = document.querySelectorAll('.client');
+        let inputsQuantity = 0;
         if (window.innerWidth >= 992) {
             inputsQuantity = Math.ceil(clients.length / 3);
         }
         else if (window.innerWidth < 992) {
             inputsQuantity = Math.ceil(clients.length / 2);
         }
-        var feedback = document.querySelector('.feedback-inputs');
+        const feedback = document.querySelector('.feedback-inputs');
 
         feedback.innerHTML = '';
 
-        for (var i = 0; i < inputsQuantity; i++) {
+        for (let i = 0; i < inputsQuantity; i++) {
             if (i === 0) {
                 feedback.insertAdjacentHTML('beforeEnd',
                     '<input type="radio" name="tabs" checked id="tab-' + (i + 1) + '"><label for="tab-' + (i + 1) + '"></label>')
@@ -92,39 +92,35 @@ window.onload = function () {
         }
     }
 
-    // getInputs();
-    // addClickEventOnInputs();
-
     function addClickEventOnInputs() {
 
-        var inputs = document.querySelectorAll('.feedback-inputs>input');
+        const inputs = document.querySelectorAll('.feedback-inputs>input');
 
-        for (var i = 0; i < inputs.length; i++) {
+        for (let i = 0; i < inputs.length; i++) {
             inputs[i].addEventListener('click', getFeedback);
         }
 
     }
 
     function getFeedback() {
-        debugger;
-        var id = this.getAttribute('id').replace('tab-', '') || 1;
+        const id = this.getAttribute('id').replace('tab-', '') || 1;
         if (window.innerWidth >= 992) {
-            var columns = document.querySelectorAll('.clients>.col-lg-4');
-            for (var i = 0; i < columns.length; i++) {
+            const columns = document.querySelectorAll('.clients>.col-lg-4');
+            for (let i = 0; i < columns.length; i++) {
                 columns[i].style.display = 'none';
             }
-            for (var i = id * 3 - 3; i < id * 3; i++) {
+            for (let i = id * 3 - 3; i < id * 3; i++) {
                 if (columns.length > i) {
                     columns[i].style.display = 'block';
                 }
             }
         }
         else if (window.innerWidth < 992) {
-            var columns = document.querySelectorAll('.clients>.col-md-6');
-            for (var i = 0; i < columns.length; i++) {
+            const columns = document.querySelectorAll('.clients>.col-md-6');
+            for (let i = 0; i < columns.length; i++) {
                 columns[i].style.display = 'none';
             }
-            for (var i = id * 2 - 2; i < id * 2; i++) {
+            for (let i = id * 2 - 2; i < id * 2; i++) {
                 if (columns.length > i) {
                     columns[i].style.display = 'block';
                 }
@@ -133,47 +129,46 @@ window.onload = function () {
     }
 
     function getFeedbackOnResize() {
-        debugger;
-        var id = 1;
+        let id = 1;
         if (window.innerWidth >= 992) {
-            var columns = document.querySelectorAll('.clients>.col-lg-4');
+            const columns = document.querySelectorAll('.clients>.col-lg-4');
 
-            for (var i = 0; i < columns.length; i++) {
+            for (let i = 0; i < columns.length; i++) {
                 if (columns[i].style.display === 'block') {
                    id = Math.ceil((i + 1) / 3);
-                   var tab = '#tab-' + id;
+                   const tab = '#tab-' + id;
                    document.querySelector(tab).setAttribute('checked', '');
                    break;
                 }
             }
 
-            for (var i = 0; i < columns.length; i++) {
+            for (let i = 0; i < columns.length; i++) {
                 columns[i].style.display = 'none';
             }
 
-            for (var i = id * 3 - 3; i < id * 3; i++) {
+            for (let i = id * 3 - 3; i < id * 3; i++) {
                 if (columns.length > i) {
                     columns[i].style.display = 'block';
                 }
             }
         }
         else if (window.innerWidth < 992) {
-            var columns = document.querySelectorAll('.clients>.col-md-6');
+            const columns = document.querySelectorAll('.clients>.col-md-6');
 
-            for (var i = 0; i < columns.length; i++) {
+            for (let i = 0; i < columns.length; i++) {
                 if (columns[i].style.display === 'block') {
                     id = Math.ceil((i + 1) / 2);
-                    var tab = '#tab-' + id;
+                    const tab = '#tab-' + id;
                     document.querySelector(tab).setAttribute('checked', '');
                     break;
                 }
             }
 
-            for (var i = 0; i < columns.length; i++) {
+            for (let i = 0; i < columns.length; i++) {
                 columns[i].style.display = 'none';
             }
 
-            for (var i = id * 2 - 2; i < id * 2; i++) {
+            for (let i = id * 2 - 2; i < id * 2; i++) {
                 if (columns.length > i) {
                     columns[i].style.display = 'block';
                 }
